a6bf675d868f56061de5c5456e3319b96baf8bb3
cem istanbullu <istanbullucem@gmail.com>
2021-05-11 23:30:18 +0300

    message

---
+++ deneme.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function deneme() {
+    return (
+        <View>
+            <Text>'efvefe'</Text>
+        </View>
+    )
+}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function App() {
+export default function index() {
 
-    let countries = ['Turkey','usa','almanya','kanada'];
+    let countries = ['Turkey112','usa121','alm23anya','kanada'];
 
     return (
         <View>
@@ -11,5 +11,5 @@ export default function App() {
                 countries.map((item) => (<Text>{item}</Text>))
             }
         </View>
-    )
-};
+    );
+}
