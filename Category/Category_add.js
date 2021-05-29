7455fbd584e3977a76c5f2ca6e443c7c6f6bd794
berkcatalahmetoglu <45469910+berkcatalahmetoglu@users.noreply.github.com>
2021-05-29 15:10:37 +0300

    Homescreen, Categorylist, CategoryAdd arayüz

--- Category/Category_add.js
+++ Category/Category_add.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Button } from 'react-native';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 import { Input } from 'react-native-elements';
 import config from '../env/config'
 import basemanager from '../service/baseservice';
@@ -29,23 +29,145 @@ const Category_add = () => {
 
 
     return (
-        <View>
+        <View style={styles.container}>
+            <ImageBackground source={require('../Images/background.jpg')} style={styles.image} >
 
-            <Input
-                placeholder='Category name'
-                onChangeText={value => setCategoryName(value)}
-            />
+                <View>
+                    <Input style={styles.textInput}
+                        underlineColorAndroid='transparent'
+                        placeholder='  Category name'
+                        onChangeText={value => setCategoryName(value)}
+                    />
+                    <Input style={styles.textInput2}
 
-            <Input
-                placeholder='Description'
-                onChangeText={value => setDescription(value)}
-            />  
+                        multiline
+                        numberOfLines={4}
 
-            <Button onPress={() => send()} title='Send'></Button>
+                        placeholder='Description'
+
+                        onChangeText={value => setDescription(value)}
+                    />
 
 
-        </View>
+                </View>
+
+
+                <View style={styles.fixToText}>
+                  
+                    <Button
+                        onPress={() => send()} title="Send!"
+                      
+                        color="#8B75B6"
+                    />
+                </View>
+
+
+            </ImageBackground>
+        </View >
+
     )
 }
 
+
+
+
+
 export default Category_add
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: '100%',
+        height: '100%',
+
+
+    },
+    fixToText: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginHorizontal:350,
+      },
+
+
+
+    sendButton: {
+        backgroundColor: "#8B75B6",
+        borderRadius: 15,
+        width: 260,
+        height: 60,
+        textAlign: 'center',
+        paddingVertical: 20,
+        marginTop: 30,
+        marginHorizontal: 150,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        }
+    },
+
+
+    textStyle: {
+        color: "Black",
+        fontSize: 16,
+        fontWeight: 'bold',
+        fontFamily: 'Tahoma'
+
+    },
+    title: {
+        color: '#e5e5e5',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginHorizontal: 60,
+        fontFamily: 'Tahoma',
+    },
+    textInput: {
+        flex: 1,
+        backgroundColor: '#EEEDE7',
+        borderRadius: 2,
+        shadowColor: "#1D1437",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 2.82,
+        shadowRadius: 2.22,
+
+        elevation: 3,
+        marginTop: 10,
+        border: 'none',
+        overflow: 'hidden'
+
+    },
+    textInput2: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#EEEDE7',
+        borderRadius: 2,
+        shadowColor: "#1D1437",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 2.82,
+        shadowRadius: 2.22,
+
+        elevation: 3,
+        marginTop: 10,
+        border: 'none',
+        overflow: 'hidden'
+
+    },
+
+
+
+});
--- routesample/Homescreen.js
+++ routesample/Homescreen.js
@@ -1,31 +1,162 @@
 import React from 'react'
-import { Button } from 'react-native'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableHighlight, TouchableOpacity, ImageBackground } from 'react-native'
+import { Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+
+
 
 export default function Homescreen({ navigation }) {
 
 
     const goContact = () => {
-       //  navigation.navigate('Contact');
+        //  navigation.navigate('Contact');
         navigation.push('order');
     }
 
-    const goProducts = () =>{
+    const goProducts = () => {
         navigation.navigate('Products')
     }
 
-    const goCategory = () =>{
+    const goCategory = () => {
         navigation.navigate('Category')
     }
 
+
     return (
-        <View>
-            
-            <Text>Home Screen!</Text>
-            <Button onPress={() => goContact()} title='Go to Order!'></Button>
-            <Button onPress={() => goProducts()} title='Go to Products!'></Button>
-            <Button onPress={() => goCategory()} title='Go to Categories!'></Button>
+        <View style={styles.container} >
+
+            <ImageBackground source={require('../Images/background.jpg')} style={styles.image}>
+
+
+
+
+                <View><Text style={styles.title}>  Home Screen!</Text></View>
+
+
+
+
+                <View>
+                    <TouchableOpacity onPress={() => goContact()}>
+                        <View style={styles.viewBtn1}>
+
+                            <Text style={styles.textStyle}>Go to Order!</Text>
+
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={() => goProducts()}>
+                        <View style={styles.viewBtn2}>
+                            <Text style={styles.textStyle}>Go to Products!</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View >
+                    <TouchableOpacity onPress={() => goCategory()}>
+                        <View style={styles.viewBtn3}>
+                            <Text style={styles.textStyle}>Go to Categories!</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+
+            </ImageBackground>
+
+
 
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        width: '100%',
+        height: '100%'
+    },
+
+    viewBtn1: {
+        backgroundColor: "#1D1437",
+        borderRadius: 15,
+        width: 260,
+        height: 60,
+        textAlign: 'center',
+        paddingVertical: 20,
+        marginTop: 30,
+        marginHorizontal: 58,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 4.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+
+    },
+    viewBtn2: {
+        backgroundColor: "#8B75B6",
+        borderRadius: 15,
+        width: 260,
+        height: 60,
+        textAlign: 'center',
+        paddingVertical: 20,
+        marginTop: 30,
+        marginHorizontal: 58,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 4.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+
+    },
+    viewBtn3: {
+        backgroundColor: "#6F508E",
+        borderRadius: 15,
+        width: 260,
+        height: 60,
+        textAlign: 'center',
+        paddingVertical: 20,
+        marginTop: 30,
+        marginHorizontal: 58,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 4.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+
+    },
+
+    textStyle: {
+        color: "#e5e5e5",
+        fontSize: 16,
+        fontWeight: 'bold',
+        fontFamily: 'Tahoma'
+
+    },
+    title: {
+        color: '#e5e5e5',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginHorizontal: 60,
+        fontFamily: 'Tahoma',
+    },
+
+
+});
